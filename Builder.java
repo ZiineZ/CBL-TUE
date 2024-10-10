df96da4636ebc88c6ae85ffe0adc3378979867b1
ZiineZ <stan.theunissen@ziggo.nl>
2024-10-10 10:51:59 +0200

    Base files
    Added base files to render the layout to a window

---
+++ Builder.java
@@ -0,0 +1,8 @@
+public class Builder {
+    
+    public static void main(String[] args) {
+        new MyWindow(); 
+        System.out.println(args);
+    }
+
+}
---
+++ MyWindow.java
@@ -0,0 +1,51 @@
+import javax.swing.*;
+import Handlers.*;
+import java.awt.*;
+
+
+public class MyWindow extends JFrame {
+
+    final JPanel visualgui = new JPanel();
+    final JPanel terminalgui = new JPanel();
+    final JPanel scoregui = new JPanel();
+
+    MyWindow(){
+        this.setTitle("TTC-300");
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setResizable(false);
+        this.setSize(960, 540);
+
+        this.getContentPane().setBackground(Color.WHITE);
+
+        initPanels(this);
+        this.setVisible(true);
+    }
+
+    void initPanels(JFrame frame){
+        final int margin = 3;
+
+        visualgui.setBounds(0+margin,0+margin,(int)(frame.getWidth()/1.5)-margin,(frame.getHeight()/2) - margin);
+        terminalgui.setBounds(0+margin, (frame.getHeight() / 2) + margin, (int) (frame.getWidth() * 0.6667) - margin, (frame.getHeight() / 2) - 11*margin);
+        scoregui.setBounds((int) (frame.getWidth() * 0.6667)+margin, 0 + margin, (int) (frame.getWidth() * 0.3334) - 2*margin, frame.getHeight() - 11*margin);
+
+        visualgui.setBackground(Color.RED);
+        terminalgui.setBackground(Color.GREEN);
+        scoregui.setBackground(Color.BLUE);
+
+        visualgui.setVisible(true);
+        terminalgui.setVisible(true);
+        scoregui.setVisible(true);
+
+
+        frame.add(visualgui);
+        frame.add(terminalgui);
+        frame.add(scoregui);
+
+        addHandlers();
+    }
+
+    void addHandlers() {
+        terminalgui.add(new terminalHandler(terminalgui));
+    }
+    
+}
