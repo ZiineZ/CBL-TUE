5de1b5950b85a9c6eed285cb9e7574ea1f8aac1f
ZiineZ <stan.theunissen@ziggo.nl>
2024-10-10 10:53:01 +0200

    Implemented Handlers Base files
    Filled terminalHandler with base

---
+++ Handlers/terminalHandler.java
@@ -0,0 +1,47 @@
+package Handlers;
+
+import javax.swing.*;
+
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.event.*;
+
+public class terminalHandler extends JTextArea implements ActionListener, FocusListener {
+
+    public terminalHandler(JPanel parentPanel) {
+        this.setPreferredSize(new Dimension(parentPanel.getWidth(), parentPanel.getHeight()));
+        this.setBackground(Color.GREEN);
+        this.setOpaque(true);
+        this.setText("Lorem ipsum dolor sit amet, consectetur adipiscing elit. "
+                + "Pellentesque vitae velit ex.");
+
+        JScrollPane scrollPane = new JScrollPane(this);
+        scrollPane.setPreferredSize(new Dimension(parentPanel.getWidth(), parentPanel.getHeight()));
+
+        parentPanel.setLayout(new BorderLayout());
+        parentPanel.add(scrollPane, BorderLayout.CENTER);
+
+        parentPanel.revalidate();
+        parentPanel.repaint();
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'actionPerformed'");
+    }
+
+    @Override
+    public void focusGained(FocusEvent e) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'focusGained'");
+    }
+
+    @Override
+    public void focusLost(FocusEvent e) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'focusLost'");
+    }
+
+}
